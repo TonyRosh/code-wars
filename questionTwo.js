ec2a32b02b5cefb6e73121aecbf466156a01996c
“Tony <adrprogramming@gmail.com>
2021-08-24 16:12:20 -0600

    third question

--- questionTwo.js
+++ questionTwo.js
@@ -10,6 +10,7 @@
 
 // ["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
 // The elements in the returned array should be in the same order as in the initial array passed to your function, albeit with the 'geese' removed. Note that all of the strings will be in the same case as those provided, and some elements may be repeated.
+// kata 7
 
 function gooseFilter (birds) {
     let geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
